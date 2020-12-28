2adb945446af2ffa604f1d9dd8510a92ab45275b
Mufaro Simbisayi <mufaro@wethinkcode.co.za>
2020-12-28 15:57:25 +0200

    beginnings of a weather client

---
+++ 05_weather_client/program.py
@@ -0,0 +1,8 @@
+print('###########################################################')
+print('                                                           ')
+print('            Author: Mufaro Simbisayi                       ')
+print('                                                           ')
+print('###########################################################')
+
+import requests
+from bs4 import BeautifulSoup
